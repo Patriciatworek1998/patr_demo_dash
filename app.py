9141cc013ee93dddcdd8f499f6fa5621972da9aa
Patriciatworek1998 <patriciatworek@live.dk>
2022-03-28 10:32:28 +0200

    f

--- app.py
+++ app.py
@@ -1,33 +1,21 @@
-# ***************************************
-# Imports
-# ***************************************
-# Dash
 import dash
 from dash import html
 from dash import dcc
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
-# Div.
 import pandas as pd
 import numpy as np
 import calendar
 
-# Plotly
 import plotly.express as px
 import plotly.graph_objects as go
 
-# ***************************************
-# Get data
-# ***************************************
 import datamodel
 order = datamodel.get_data()
 df_year = datamodel.get_year()
 df_month = datamodel.get_month()
 
-# ***************************************
-# Diagram - Employee Sales
-# ***************************************
 fig_employee = px.bar(order, 
     x='emp_name', y='total', 
     color='type', text='total', title='SALES BY EMPLOYEE',
@@ -48,32 +36,14 @@ fig_products = px.bar(order,
 fig_products.update_traces(texttemplate='%{text:.2s}', textposition='outside')
 fig_products.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
 
-# ***************************************
-# Activate the app
-# ***************************************
-#app = dash.Dash(__name__)
-
 dash_app = dash.Dash(__name__)
 app = dash_app.server
 
-
-#dash_app = dash.Dash(__name__)
-#app = dash_app.server
-
-
-
-# ***************************************
-# Layout
-# ***************************************
 dash_app.layout = html.Div(style={'backgroundColor':'lightgrey'},
     children=[
         html.Div(className='row',
                 children=[
-                    #html.Div(className='four columns div-user-controls',
-                            #children=[
-                                #html.H2('Sales dashboard',style={'color':'blue','textAlign':'center'}),
-                                #html.P('Employee and Product', style={'textAlign':'center','color':'blue'}),
-                    html.Div(#className='eight columns div-for-charts bg-grey',
+                    html.Div(
                             children=[
                                 dcc.Graph(id="sales_employee", figure=fig_employee),
                                 dcc.Graph(id="sales_product", figure=fig_products)])
@@ -81,56 +51,5 @@ dash_app.layout = html.Div(style={'backgroundColor':'lightgrey'},
                     ),
                 ]
         )
-    #]
-#)
-                    
-                
-                
-        
-    
-
-    
-                
-    
-
-
-# ***************************************
-# Callbacks
-# ***************************************
-# Output er diagrammet
-# Input er DropDown
-
-
-
-#@dash_app.callback(Output('sales_employee', 'figure'),
-              #[Input('drop_month', 'value')],
-              #[Input('drop_year', 'value')])
-
-#def update_graph(drop_month, drop_year):
-    #if drop_year:
-        #if drop_month:
-            #Data i både drop_month og drop_year
-            #order_fig1 = order.loc[(order['orderyear'] == drop_year) & (order['ordermonth'] == drop_month)]
-        #else:
-            #Data i drop_year. men ikke drop_month
-            #order_fig1 = order.loc[order['orderyear'] == drop_year]
-    #else:
-        #if drop_month:
-            #Data i drop_month, men ikke drop_year
-            #order_fig1 = order.loc[order['ordermonth'] == drop_month]
-        #else:
-            #Ingen data - ikke noget valgt
-            #order_fig1 = order
-        
-    #return {'data':[go.Bar(
-        #x = order_fig1['productname'],
-        #y = order_fig1['total']
-            #)
-        #]
-    #c}
-
-# ***************************************
-# Run the app
-# ***************************************
 if __name__ == '__main__':
     dash_app.run_server(debug=True)
